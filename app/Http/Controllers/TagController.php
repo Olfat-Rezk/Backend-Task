d2b890f4a84a6bcc5cb8f9894f285b723b2a56b7
Olfat-Rezk <84644058+Olfat-Rezk@users.noreply.github.com>
2023-01-13 06:40:34 +0200

    first commit

---
+++ app/Http/Controllers/TagController.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Tag;
+use Illuminate\Http\Request;
+
+class TagController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $tags = Tag::all();
+        return response($tags) ;
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'name'=>'required|string|unique:tags,name'
+        ]);
+        $tags = Tag::create($request->all());
+        return response($tags);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $tags = Tag::findOrFail($id);
+        return response($tags);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $tags = Tag::findOrFail($id);
+        $tags->update($request->all());
+
+        return response($tags);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+       $tags= Tag->destroy($id);
+       return response($tags);
+
+    }
+}
---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use App\Http\Requests\AuthRequest;
+use Illuminate\Support\Facades\Hash;
+
+
+
+class UserController extends Controller
+{
+ public function register(AuthRequest $request){
+    $validated = $request->validate();
+    $user = User::create([
+        'name'=>$validated['name'],
+        'phone'=>$validated['phone'],
+        'password'=>bcrypt($validated['password'])
+    ]);
+    $token = $user->createToken('myapptoken')->plainTextToken;
+    $response =[
+        'user'=> $user,
+        'token'=> $token
+    ];
+    return response($response,201);
+
+ }
+
+public function login(Request $request)
+{
+    $validated = $request->validate([
+        'phone' =>'required|numeric|regex:01[0125][0-9]{8}',
+        'password'=>'required|string'
+    ]);
+
+    $user = User::where('phone',$validated['phone'])->first();
+    if(! $user || !Hash::check($validated['password'],$user->password)){
+        return response([
+            'message'=>'error phone or password please try again'
+        ],401);}
+        $token = $user ->createToken('myapptoken')->plainTextToken;
+        $response = [
+            'user'=> $user,
+            'token'=> $token
+        ];
+        return response($response,201);
+
+
+    }
+    public function logout(Request $request)
+    {
+        $request->user()->currentAccessToken()->delete();
+        return [
+            'message'=>'logout'
+        ];
+
+    }
+
+}
